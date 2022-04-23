060205a550ca7a7a167f6259cd80cddce5b0d9a1
Daniil Levchenko <97levchenko.danya@gmail.com>
2022-04-23 03:48:24 +0300

    [upd]

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from pydantic import HttpUrl
 from sqlmodel import create_engine, Session, SQLModel, Field
-DB_URL = "postgresql+psycopg2://postgres:postgres@database:5432/web_scrapper"
+
+DB_URL = "postgresql+psycopg2://postgres:postgres@0.0.0.0:5435/web_scrapper"
 engine = create_engine('sqlite:///data.db', connect_args={'check_same_thread': False})
 
 
@@ -18,12 +19,19 @@ class News(SQLModel, table=True):
             return self
 
 
+def db_init():
+    for i in range(6):
+        logger.info(msg='Processing...')
+        sleep(1)
+    SQLModel.metadata.create_all(engine) # TODO fix docker doesn't create tables
+    logger.info(msg='Tables initialized')
+
+
 if __name__ == '__main__':
     import os
     import logging
+    from time import sleep
 
     logging.basicConfig(level=10)
     logger = logging.getLogger('DATABASE')
-
-    SQLModel.metadata.create_all(engine)
-    logger.info(msg='Tables initialized')
+    db_init()
