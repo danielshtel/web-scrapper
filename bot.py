551f9ce3d9f9a911de8b90f36c008843f0505439
Daniil Levchenko <97levchenko.danya@gmail.com>
2022-04-25 19:36:40 +0300

    [add] logging -> bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
+import logging
+
 import telebot
 
 from config import settings
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('BOT')
 TOKEN = settings.BOT_TOKEN
 bot = telebot.TeleBot(TOKEN)
 
-
-
-
 if __name__ == '__main__':
+    logger.info('BOT STARTED')
     bot.infinity_polling()
-
