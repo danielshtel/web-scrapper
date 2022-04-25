77003f79bda716f42f012d6a08ff4b61f1ed6e89
Daniil Levchenko <97levchenko.danya@gmail.com>
2022-04-25 19:37:34 +0300

    [upd] reworked SentArticles model logic

--- models.py
+++ models.py
@@ -1,7 +1,5 @@
-from typing import List
-
 from pydantic import HttpUrl
-from sqlmodel import SQLModel, Field, Session, select, ARRAY, String, Column
+from sqlmodel import SQLModel, Field, Session, select
 
 from database import engine
 
@@ -29,33 +27,23 @@ class News(SQLModel, table=True):
 class SentArticles(SQLModel, table=True):
     __tablename__ = 'sent_articles'
     id: int | None = Field(None, primary_key=True)
-    article_url: List[str] = Field(index=True)  # TODO array
+    url: HttpUrl = Field(index=True, sa_column_kwargs={'unique': True})
     _session: Session = Session(engine)
 
     @classmethod
-    def get_sent_articles_urls(cls) -> list:
-        sent_articles = cls._session.exec(select(cls)).one().article_url
-        return sent_articles
+    def create(cls, url):
+        with cls._session as session:
+            obj = cls(url=url)
+            session.add(obj)
+            session.commit()
+            session.refresh(obj)
+            return obj
 
     @classmethod
-    def initialize_storage(cls):
-        obj = cls._session.exec(select(SentArticles)).first()
-        if obj is None:
-            obj = cls(article_url=[])
-            cls._session.add(obj)
-            cls._session.commit()
-            cls._session.refresh(obj)
-
-        if cls._session.exec(select(SentArticles)).first().id == 1:
-            return
-
-    def update(self, data):
-        with self._session as session:
-            self.article_url.extend(data)
-            session.add(self)
-            session.commit()
-            session.refresh(self)
+    def get_all(cls) -> list:
+        articles = cls._session.exec(select(SentArticles)).all()
+        return articles
 
 
 if __name__ == '__main__':
-    SentArticles.initialize_storage()
+    pass
