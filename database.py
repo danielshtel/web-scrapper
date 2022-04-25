4d5a474d544c6a1dcbfe9ead426ea5a30948d940
Daniil Levchenko <97levchenko.danya@gmail.com>
2022-04-25 22:30:55 +0300

    [upd] add sleep

--- database.py
+++ database.py
@@ -5,9 +5,6 @@ engine = create_engine(DB_URL)
 
 
 def db_init():
-    # for i in range(6):
-    #     logger.info(msg='Processing...')
-    #     sleep(1)
     SQLModel.metadata.create_all(engine)
     logger.info(msg='Tables initialized')
 
@@ -16,7 +13,7 @@ if __name__ == '__main__':
     from models import (News, SentArticles)
     import logging
     from time import sleep
-
     logging.basicConfig(level=10)
     logger = logging.getLogger('DATABASE')
+    sleep(5)
     db_init()
