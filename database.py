cb3e276cf445b60fa0360bc67eeaaacf5fed2b17
Daniil Levchenko <97levchenko.danya@gmail.com>
2022-04-26 17:33:47 +0300

    [upd] add check 'if tables exists'

--- database.py
+++ database.py
@@ -1,18 +1,22 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, inspect
 
 DB_URL = "postgresql+psycopg2://postgres:postgres@database:5432/web_scrapper"
 engine = create_engine(DB_URL)
 
 
 def db_init():
-    SQLModel.metadata.create_all(engine)
-    logger.info(msg='Tables initialized')
+    if not inspect(engine).has_table('news') and not inspect(engine).has_table('sent_articles'):
+        SQLModel.metadata.create_all(engine)
+        logger.info(msg='Tables initialized')
+    else:
+        logger.info(msg='Tables exists')
 
 
 if __name__ == '__main__':
-    from models import (News, SentArticles)
+    from models import News, SentArticles
     import logging
     from time import sleep
+
     logging.basicConfig(level=10)
     logger = logging.getLogger('DATABASE')
     sleep(5)
